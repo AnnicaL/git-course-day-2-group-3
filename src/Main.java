737657d18e9b96e19b3f61379ed36bd77b3727b0
Annica Landbü <annica.landbu@hm.com>
2021-04-12 10:39:03 +0200

    # Please enter the commit message for your changes. Lines starting

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import static java.lang.System.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 5;
+
+        adding(x, y);
+
+    }
+    public static void adding(int x, int y){
+           int result = x + y;
+            out.println(result);
+
+        }
+    }
+
